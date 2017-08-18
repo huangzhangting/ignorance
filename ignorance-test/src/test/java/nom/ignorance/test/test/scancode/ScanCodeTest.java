10de65b30ef48ee742604fa41863a8ff9df424fc
huangzhangting <huangzhangting@wdai.com>
2017-08-18 10:27:47 +0800

    code scan test

--- ignorance-test/src/test/java/nom/ignorance/test/test/scancode/ScanCodeTest.java
+++ ignorance-test/src/test/java/nom/ignorance/test/test/scancode/ScanCodeTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by huangzhangting on 2017/8/17.
@@ -11,31 +13,48 @@ import java.io.*;
 @Slf4j
 public class ScanCodeTest {
     private int codeLineCount = 0;
+    private int onlyBraceCount = 0;
+
+    private static final String JAVA_FILE = ".java";
+
+    private static List<String> ignoreDirectoryList = new ArrayList<String>(){{
+        add("test");
+        add("target");
+
+    }};
+
+    private static List<String> ignoreFileList = new ArrayList<String>(){{
+        add("OSSUtil");
+        add("SequenceNumberUtil.java");
+
+    }};
+
 
     @Test
-    public void test_1() throws Exception{
+    public void test_scan() throws Exception{
         String path = "D:\\weidai\\project\\weidai\\wpai-common";
         File file = new File(path);
 
         if(file.isDirectory()){
             handleDirectory(file);
             log.info("代码行数：{}", codeLineCount);
+            log.info("大括弧占一行数：{}", onlyBraceCount);
         }else{
             log.info(file.getName() + " 不是文件夹");
         }
     }
 
     private void handleDirectory(File directory){
+        String name = directory.getName();
+        if(ignoreDirectoryList.contains(name)){
+            log.info("忽略文件夹：{}", name);
+            return;
+        }
         File[] files = directory.listFiles();
         if(files==null || files.length==0){
             log.info(directory.getName()+" 是空文件夹");
             return;
         }
-        handleFileArray(files);
-    }
-
-    private void handleFileArray(File[] files){
-//        System.out.println(files.length);
         for(File f : files){
             handleFile(f);
         }
@@ -55,11 +74,17 @@ public class ScanCodeTest {
             log.info(name+" 不是文件");
             return;
         }
-        if(!name.endsWith(".java")){
+        if(!name.endsWith(JAVA_FILE)){
             return;
         }
-        System.out.println(name);
+        int idx = name.indexOf(JAVA_FILE);
+//        System.out.println(name+" ---- "+name.substring(0, idx));
+        if(ignoreFileList.contains(name.substring(0, idx)) || ignoreFileList.contains(name)){
+            log.info("忽略文件：{}", name);
+            return;
+        }
 
+        //fixme 测试
         if(name.startsWith("MD5Util")){
             try {
                 readJava(file);
@@ -73,12 +98,58 @@ public class ScanCodeTest {
     private void readJava(File file) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
         String line;
+        boolean isDocNote = false; // 文档注释
+        boolean isMultiNote = false; // 多行注释
         while ((line=reader.readLine()) != null){
+            if(isDocNote){
+                if(isMultiNoteEnd(line)){
+                    isDocNote = false;
+                }
+                continue;
+            }
+            if(isMultiNote){
+                if(isMultiNoteEnd(line)){
+                    isMultiNote = false;
+                }
+                continue;
+            }
+            isDocNote = isDocNoteStart(line);
+            if(!isDocNote){
+                isMultiNote = isMultiNoteStart(line);
+            }
+            if(isDocNote || isMultiNote || isLineNote(line) || isBr(line)){
+                continue;
+            }
+            calculateCodeLineCount(line);
+
             System.out.println(line);
-            codeLineCount++;
+
         }
     }
 
+    private void calculateCodeLineCount(String line){
+        String str = line.trim();
+        if(str.equals("{") || str.equals("}")){
+            onlyBraceCount++;
+            return;
+        }
+        codeLineCount++;
+    }
 
+    private boolean isDocNoteStart(String line){
+        return line.trim().startsWith("/**");
+    }
+    private boolean isMultiNoteEnd(String line){
+        return line.trim().endsWith("*/");
+    }
+    private boolean isMultiNoteStart(String line){
+        return line.trim().startsWith("/*");
+    }
+    private boolean isLineNote(String line){
+        return line.trim().startsWith("//");
+    }
+    private boolean isBr(String line){
+        return line.trim().equals("");
+    }
 
 }
